cf504b7b0bcd80087142c64ce8067b70720f8094
Pham Kim Hoan <pham0030@gmail.com>
2018-12-03 07:23:23 +0000

    add cors

--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,19 @@
 var express = require('express');
 const bodyParser = require('body-parser');
-var User = require('../models/users');
 var passport = require('passport');
 var authenticate = require('../authenticate');
+var User = require('../models/users');
+const cors = require('./cors');
 
 var router = express.Router();
 router.use(bodyParser.json());
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', cors.corsWithOptions, function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/signup', (req, res, next) => {
+router.post('/signup', cors.corsWithOptions, (req, res, next) => {
   User.register(new User({username: req.body.username}),
     req.body.password, (err, user) => {
     if(err) {
@@ -42,7 +43,7 @@ router.post('/signup', (req, res, next) => {
   });
 });
 
-router.post('/login', passport.authenticate('local'), (req, res) => {
+router.post('/login', cors.corsWithOptions, passport.authenticate('local'), (req, res) => {
   var token = authenticate.getToken({_id: req.user._id});
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
